21142c5c189e8fc595562e538fb994d4e343d02c
cactus39 <106186411+cactus39@users.noreply.github.com>
2023-11-03 11:51:17 +0100

    Add possible netmiko argument read_timeout_override

--- napalm_procurve/procurve.py
+++ napalm_procurve/procurve.py
@@ -69,6 +69,7 @@ class ProcurveDriver(NetworkDriver):
             "alt_host_keys": False,
             "alt_key_file": "",
             "ssh_config_file": None,
+            "read_timeout_override": None,
         }
 
         # Build dict of any optional Netmiko args
